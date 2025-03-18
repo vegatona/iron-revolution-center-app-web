b8948de731b18451d160b67b4b71604a5bcfc5fc
JuandaMonkey <juandanieldecide@gmail.com>
2025-03-18 17:21:58 -0600

    feat(app): agregado de las rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,5 +6,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes)]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainDashboardComponent } from '../component/dashboard/main-dashboard.component';
+import { StatisticsOverviewComponent } from '../component/statistics/statistics-overview.component';
+import { ClientsManagementComponent } from '../component/clients/clients-management.component';
+import { MembershipsManagementComponent } from '../component/memberships/memberships-management.component';
+import { ExercisesManagementComponent } from '../component/exercises/exercises-management.component';
+import { EmployeesManagementComponent } from '../component/employees/employees-management.component';
+import { BranchesManagementComponent } from '../component/branches/branches-management.component';
+import { UsersManagementComponent } from '../component/users/users-management.component';
+
+export const routes: Routes = [
+    { path: 'main-dashboard', component: MainDashboardComponent },
+    { path: 'statics', component: StatisticsOverviewComponent},
+    { path: 'clients-management', component: ClientsManagementComponent},
+    { path: 'memberships-management', component: MembershipsManagementComponent },
+    { path: 'exercises-management', component: ExercisesManagementComponent },
+    { path: 'employees-management', component: EmployeesManagementComponent },
+    { path: 'branches-management', component: BranchesManagementComponent },
+    { path: 'users-management', component: UsersManagementComponent }
+];
